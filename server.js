ff3ca22c0215693c0e0679c38c73531de97c3e0d
Pagidimarri Sravani <pagidimarrisravani56@gmail.com>
2025-10-02 22:01:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 8080;
+
+http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'application/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+  };
+
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if(error.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found</h1>', 'utf-8');
+      }
+      else {
+        res.writeHead(500);
+        res.end(`Server Error: ${error.code}`);
+      }
+    }
+    else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+}).listen(port);
+
+console.log(`Server running at http://localhost:${port}/`);
